198aae870d257a4f93566f2039a434eb544a870c
Robbert van 't Westende <rvtwestende@totalactivemedia.nl>
2014-11-09 00:40:15 +0100

    Add graph on user dash.

--- sites/all/themes/energappit/assets/javascripts/script.js
+++ sites/all/themes/energappit/assets/javascripts/script.js
@@ -7,26 +7,29 @@
 
   $document.ready(function() {
     $document.resize(function() {
-      var $pane2 = $('.pane-fpid-2'),
-        voffset = $pane2.offset().left,
-        $spread = $('<div class="spread"><div class="container"></div></div>');
 
-      $pane2.css({
-        left: - voffset + 'px',
-        marginRight: - (2*voffset) + 'px',
-        height: $( window ).height()
-      });
+      if ($('body').hasClass('front')) {
+        var $pane2 = $('.pane-fpid-2'),
+          voffset = $pane2.offset().left,
+          $spread = $('<div class="spread"><div class="container"></div></div>');
 
-      $spread.css({
-        left: - voffset + 'px',
-        marginRight: - (2*voffset) + 'px'
-      });
+        $pane2.css({
+          left: - voffset + 'px',
+          marginRight: - (2*voffset) + 'px',
+          height: $( window ).height()
+        });
 
-      $('body.front .container .row:eq(2)').wrap($spread);
+        $spread.css({
+          left: - voffset + 'px',
+          marginRight: - (2*voffset) + 'px'
+        });
 
-      $('#mini-panel-incentive_1').waypoint(function() {
-        $('#mini-panel-incentive_1 .pane-fpid-7 img').addClass('show');
-      }, { offset: '75%' });
+        $('body.front .container .row:eq(2)').wrap($spread);
+
+        $('#mini-panel-incentive_1').waypoint(function() {
+          $('#mini-panel-incentive_1 .pane-fpid-7 img').addClass('show');
+        }, { offset: '50%' });
+      }
 
     });
 
