3f7b9c05aa7572a0aab19bda596a1f16c9346787
Robbert van 't Westende <rvtwestende@totalactivemedia.nl>
2014-11-09 00:49:09 +0100

    Hardcode footer.

--- sites/all/themes/energappit/templates/page/page.tpl.php
+++ sites/all/themes/energappit/templates/page/page.tpl.php
@@ -81,7 +81,7 @@
 <footer id="footer" class="footer" role="footer">
   <div class="container">
     <?php if ($copyright): ?>
-      <small class="copyright pull-left"><?php print $copyright; ?></small>
+      <small class="copyright pull-left"><p><a href="/">Privacy Policy</a> | <a href="/">Terms of Service</a> | <a href="/">Service Level Agreement</a></p></small>
     <?php endif; ?>
     <small class="pull-right"><a href="#"><?php print t('Back to Top'); ?></a></small>
   </div>
